1d39ed6547ed22f6499f7537b2ec0fdb1419a6ce
Wei Chen <weichen@narvii.com>
2019-05-22 19:38:22 +0800

    temp commit for video encoder, can get video, but result is strange

--- app/src/main/cpp/encoder/video_encoder.cpp
+++ app/src/main/cpp/encoder/video_encoder.cpp
@@ -14,8 +14,10 @@ extern "C" {
 static void encode(AVCodecContext *enc_ctx, AVFrame *frame, AVPacket *pkt,
                    FILE *outfile) {
     int ret = 0;
-    if (frame)
-    ret = avcodec_send_frame(enc_ctx, frame);
+    if (frame && frame->data != nullptr) {
+        ret = avcodec_send_frame(enc_ctx, frame);
+        __android_log_print(ANDROID_LOG_DEBUG, "video", "avcodec_send_frame %d", ret);
+    }
     if (ret < 0) {
         __android_log_print(ANDROID_LOG_DEBUG, "video", "avcodec_send_frame fail");
         return;
@@ -23,15 +25,24 @@ static void encode(AVCodecContext *enc_ctx, AVFrame *frame, AVPacket *pkt,
 
     while (ret >= 0) {
         ret = avcodec_receive_packet(enc_ctx, pkt);
-        if (ret == AVERROR(EAGAIN) || ret == AVERROR_EOF) {
-            __android_log_print(ANDROID_LOG_DEBUG, "video", "avcodec_receive_packet end %d", ret);
+//        if (ret == AVERROR(EAGAIN) || ret == AVERROR_EOF) {
+//            __android_log_print(ANDROID_LOG_DEBUG, "video", "avcodec_receive_packet end %d", ret);
+//            return;
+//        }
+        if (ret == AVERROR(EAGAIN)) {
+            ALOGD(" output is not available in the current state - user must try to send input");
+            return;
+        }
+        else if (ret == AVERROR_EOF) {
+            ALOGD(" the encoder has been fully flushed, and there will be no more output packets");
             return;
         }
         else if (ret < 0) {
             __android_log_print(ANDROID_LOG_DEBUG, "video", "avcodec_receive_packet fail");
-            return;
+            exit(1);
         }
         fwrite(pkt->data, 1, pkt->size, outfile);
+        __android_log_print(ANDROID_LOG_DEBUG, "video", "fwrite");
         av_packet_unref(pkt);
     }
 }
@@ -57,10 +68,11 @@ video_encoder::video_encoder(const char *dest, int width, int height) {
     codecContext->bit_rate = 4000000;
     codecContext->width = width;
     codecContext->height = height;
-    codecContext->time_base = (AVRational) {1, 25};
-    codecContext->framerate = (AVRational) {25, 1};
-    codecContext->gop_size = 10;
-    codecContext->max_b_frames = 1;
+    codecContext->time_base.num =1;
+    codecContext->time_base.den =30;
+//    codecContext->framerate = (AVRational) {25, 1};
+    codecContext->gop_size = 250;
+    codecContext->max_b_frames = 5;
     codecContext->pix_fmt = AV_PIX_FMT_YUV420P;
     if (codec->id == AV_CODEC_ID_H264) {
         av_opt_set(codecContext->priv_data, "preset", "slow", 0);
@@ -72,7 +84,7 @@ video_encoder::video_encoder(const char *dest, int width, int height) {
     }
 
     swsContext = sws_getContext(codecContext->width, codecContext->height, AV_PIX_FMT_RGB24, codecContext->width,
-                                codecContext->height, AV_PIX_FMT_YUV420P, 0,
+                                codecContext->height, AV_PIX_FMT_YUV420P, SWS_POINT,
                                 nullptr, nullptr, nullptr);
     ALOGD("video encoder init success")
 }
@@ -98,14 +110,18 @@ void video_encoder::encode_frame(int64_t pts) {
         return;
     }
 
-    uint8_t *pixels = nullptr;
-    glReadPixels(0, 0, codecContext->width, codecContext->height, GL_RGB, GL_UNSIGNED_BYTE, pixels);
+    GLubyte *pixels = static_cast<GLubyte *>(malloc(4 * codecContext->width * codecContext->height));
+    glReadPixels(0, 0, codecContext->width, codecContext->height, GL_RGBA, GL_UNSIGNED_BYTE, pixels);
+    if (pixels == nullptr) {
+        __android_log_print(ANDROID_LOG_DEBUG, "video", "pixels is nullptr");
+    }
+
     uint8_t *inData[1] = {pixels};
-    int inlineSize[1] = {3 * codecContext->width};
+    int inlineSize[1] = {4 * codecContext->width};
     sws_scale(swsContext, inData, inlineSize, 0, codecContext->height, frame->data, frame->linesize);
     frame->pts = pts;
     AVPacket *packet = av_packet_alloc();
     encode(codecContext, frame, packet, f);
+    av_frame_unref(frame);
 
-    __android_log_print(ANDROID_LOG_DEBUG, "video", "encode one frame");
 }
